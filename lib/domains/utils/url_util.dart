fcdb036ce94a723a1075e1361d78be2caf0acfd4
Manuel Loayza <manuelloayza@MacBook-Pro-de-Manuel.local>
2022-04-21 14:05:49 -0500

    cambios finales

--- lib/domains/utils/url_util.dart
+++ lib/domains/utils/url_util.dart
@@ -1,7 +1,7 @@
 import 'package:connectivity/connectivity.dart';
 
-//const String url = "https://ordenes.icreativa.com.ec/api/";
-const String url = "https://hungry-shrimp-93.loca.lt/api/";
+const String url = "https://ordenes.icreativa.com.ec/api/";
+//const String url = "https://hungry-shrimp-93.loca.lt/api/";
 const String direccionImagen = url + 'foto/';
 verificarConexion() async {
   var connectivityResult = await (Connectivity().checkConnectivity());
